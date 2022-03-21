160ebc85b459ea8411b663078a037cf8355b1068
Abhishek Gupta <abhishkg@thoughtworks.com>
2022-03-21 14:16:10 +0530

    Use the username from the incoming event payload

--- prepareReport.js
+++ prepareReport.js
@@ -4,17 +4,15 @@ const report = require('../report.json');
 const HEAD_COMMIT = process.env.HEAD_COMMIT;
 const TEST_REPO_NAME = process.env.TEST_REPO_NAME;
 const REPO_NAME = process.env.REPO_NAME;
+const USERNAME = process.env.USERNAME;
 
 const commit = JSON.parse(HEAD_COMMIT);
 const tests = report.tests;
 const stats = report.stats;
 
-const getUserName = function (author) {
-  return author.username ? author.username : author.name;
-};
 
 const reportWithMeta = {
-  username: getUserName(commit.author),
+  username: USERNAME,
   timestamp: commit.timestamp,
   stats,
   test_repo_name: TEST_REPO_NAME,
